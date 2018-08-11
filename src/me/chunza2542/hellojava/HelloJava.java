db97013828d61963631432fa67acec9381380c83
chunza2542 <chunza2542@hotmail.com>
2018-08-11 16:53:59 +0700

    add printMax function

--- src/me/chunza2542/hellojava/HelloJava.java
+++ src/me/chunza2542/hellojava/HelloJava.java
@@ -4,6 +4,16 @@ public class HelloJava {
 
     private static int counter = 1;
 
+    public static void printMax(int... numbers) {
+        int max = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) max = numbers[i];
+        }
+
+        System.out.println("The max number is " + max);
+    }
+
     public static void main(String[] args) {
         // create instances
         HelloJava chun = new HelloJava();
@@ -12,8 +22,11 @@ public class HelloJava {
         HelloJava steve = new HelloJava();
         steve.counter++;
 
-        // print the output
+        // print the counter output
         System.out.println("Counter: " + HelloJava.counter);
+
+        // print the max numbers
+        printMax(new int[]{10, 20, 30});
     }
 
 }
