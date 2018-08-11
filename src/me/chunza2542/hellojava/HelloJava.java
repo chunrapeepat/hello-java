3cb012068ad7acbe09ef869c7a5609969c7baed8
chunza2542 <chunza2542@hotmail.com>
2018-08-11 16:29:25 +0700

    static variable

--- src/me/chunza2542/hellojava/HelloJava.java
+++ src/me/chunza2542/hellojava/HelloJava.java
@@ -2,8 +2,18 @@ package src.me.chunza2542.hellojava;
 
 public class HelloJava {
 
+    private static int counter = 1;
+
     public static void main(String[] args) {
-       System.out.println("Hello Java my name is Chun!");
+        // create instances
+        HelloJava chun = new HelloJava();
+        chun.counter++;
+
+        HelloJava steve = new HelloJava();
+        steve.counter++;
+
+        // print the output
+        System.out.println("Counter: " + HelloJava.counter);
     }
 
 }
