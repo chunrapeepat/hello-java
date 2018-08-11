29b1ccf2373987e38f147b3a4b34232f690f04a3
chunza2542 <chunza2542@hotmail.com>
2018-08-11 15:58:59 +0700

    add HelloJava class

---
+++ src/me/chunza2542/hellojava/HelloJava.java
@@ -0,0 +1,9 @@
+package src.me.chunza2542.hellojava;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+       System.out.println("Hello Java my name is Chun!");
+    }
+
+}
